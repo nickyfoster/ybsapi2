813c131f9aa4f78aa41c739e886c6cf8e38fc021
Nikita Foster <nik9516@gmail.com>
2019-11-25 23:37:39 +0300

    test added

---
+++ test/KWE_test.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class KWE_test(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        pass
+
+    def test_KWE(self):
+        pass
--- test/first_test.py
+++ test/first_test.py
@@ -1,12 +1,12 @@
 import unittest
 
 
-class TestFisrt(unittest.TestCase):
+class TestPython(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         cls.a = 5
         cls.b = 4
 
-    def test_tfidf_kwe(self):
+    def test_python(self):
         res = self.a + self.b
         self.assertEqual(res, 9)
